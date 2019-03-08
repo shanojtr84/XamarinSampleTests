579f16aa0380d1ff18a3e6038c1e9e1c7e7f3fd3
Shanoj <shanojtr240584@outlook.com>
2019-03-08 15:10:57 +1100

    Modified post buils script

--- Src/UITestSampleApp.Droid/appcenter-post-build.sh
+++ Src/UITestSampleApp.Droid/appcenter-post-build.sh
@@ -27,7 +27,7 @@ echo ""
 
 echo "> Run UI test command"
 # Note: must put a space after each parameter/value pair
-appcenter test run uitest --app $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/com.minnick.uitestsampleapp.apk --test-series $testSeriesName --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/CrossPlatformUITests/bin/Debug --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/Src/packages/Xamarin.UITest.*/tools --token $appCenterLoginApiToken
+appcenter test run uitest --app $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/com.minnick.uitestsampleapp.apk --test-series $testSeriesName --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/Src/CrossPlatformUITests/bin/Debug --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/Src/packages/Xamarin.UITest.*/tools --token $appCenterLoginApiToken
 
 
 echo ""
